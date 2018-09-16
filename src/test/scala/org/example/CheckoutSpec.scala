b9d10474f436bc560634ffe5edf4ddfb115e890b
rwalpole <robkwalpole@gmail.com>
2018-09-16 18:25:04 +0100

    Fixed method name call

--- src/test/scala/org/example/CheckoutSpec.scala
+++ src/test/scala/org/example/CheckoutSpec.scala
@@ -8,19 +8,19 @@ class CheckoutSpec extends FlatSpec with Matchers {
   "A Checkout" should "total the cost of the items supplied in a list" in {
     val checkout = new Checkout(List[Offer]())
     val items = List[String]("apple","apple","orange")
-    checkout.AmountToPay(items) should be ("£1.45")
+    checkout.getAmountToPay(items) should be ("£1.45")
   }
 
   it should "return zero cost if the list is empty" in {
     val checkout = new Checkout(List[Offer]())
     val items = List[String]()
-    checkout.AmountToPay(items) should be ("£0.00")
+    checkout.getAmountToPay(items) should be ("£0.00")
   }
 
   it should "subtract the correct discounts when an offer is applied" in {
     val checkout = new Checkout(List[Offer](new MultiPurchaseOffer(Apple.getClass,2)))
     val items = List[String]("apple","apple")
-    checkout.AmountToPay(items) should be ("£0.60")
+    checkout.getAmountToPay(items) should be ("£0.60")
   }
 
   it should "subtract the correct discounts when multiple offers are applied" in {
@@ -28,6 +28,6 @@ class CheckoutSpec extends FlatSpec with Matchers {
       new MultiPurchaseOffer(Apple.getClass,2),
       new MultiPurchaseOffer(Orange.getClass,3)))
     val items = List[String]("apple","apple","orange","orange","orange")
-    checkout.AmountToPay(items) should be ("£1.10")
+    checkout.getAmountToPay(items) should be ("£1.10")
   }
 }
